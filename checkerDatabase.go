468858656470384b423f2dea397f802b2fc82f00
Oleksii <alex.kiselev.dev@gmail.com>
2025-09-20 11:07:19 +0100

    added some more db function, preparation before writing main functions

--- checkerDatabase.go
+++ checkerDatabase.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv" // Importing the godotenv package to load environment variables
 	_ "github.com/lib/pq"
@@ -74,4 +75,122 @@ func wowDatabase() {
 	log.Println("Successfully connected and pinged the database!")
 
 	// defer db.Close() to close the database connection when the function exits
+}
+
+func createClanMember(nickname, class, spec string) (int, error) {
+	var id int
+	query := `INSERT INTO clanMembers (nickname, class, spec) VALUES ($1, $2, $3) RETURNING member_id`
+	err := db.QueryRow(query, nickname, class, spec).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func getClanMemberByID(nickname string) (*ClanMembers, error) {
+	var member ClanMembers
+	query := `SELECT member_id, nickname, class, spec FROM clanMembers WHERE nickname = $1`
+	err := db.QueryRow(query, nickname).Scan(&member.memberID, &member.nickname, &member.class, &member.spec)
+	if err != nil {
+		return nil, err
+	}
+	return &member, nil
+}
+
+func updateClanMember(memberID int, nickname, class, spec string) error {
+	query := `UPDATE clanMembers SET nickname = $1, class = $2, spec = $3 WHERE member_id = $4`
+	_, err := db.Exec(query, nickname, class, spec, memberID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteClanMember(memberID int) error {
+	query := `DELETE FROM clanMembers WHERE member_id = $1`
+	_, err := db.Exec(query, memberID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func createRaid(dungeonName string, raidDate map[string]interface{}, raidLeaderID int) (int, error) {
+	var id int
+	raidDate = map[string]interface{}{
+		"createdAt": time.Now().Format(time.RFC3339),
+	}
+	query := `INSERT INTO raids (dungeonName, raidDate, raidLeaderID) VALUES ($1, $2, $3) RETURNING raid_id`
+	err := db.QueryRow(query, dungeonName, raidDate, raidLeaderID).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func getRaidByID(raidID int) (*Raids, error) {
+	var raid Raids
+	query := `SELECT raid_id, dungeonName, raidDate, raidLeaderID FROM raids WHERE raid_id = $1`
+	err := db.QueryRow(query, raidID).Scan(&raid.raidID, &raid.dungeonName, &raid.raidDate, &raid.raidLeaderID)
+	if err != nil {
+		return nil, err
+	}
+	return &raid, nil
+}
+
+func updateRaid(raidID int, dungeonName string, raidDate map[string]interface{}, raidLeaderID int) error {
+	query := `UPDATE raids SET dungeonName = $1, raidDate = $2, raidLeaderID = $3 WHERE raid_id = $4`
+	_, err := db.Exec(query, dungeonName, raidDate, raidLeaderID, raidID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteRaid(raidID int) error {
+	query := `DELETE FROM raids WHERE raid_id = $1`
+	_, err := db.Exec(query, raidID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func listRaids() ([]Raids, error) {
+	query := `SELECT raid_id, dungeonName, raidDate, raidLeaderID FROM raids`
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var raids []Raids
+	for rows.Next() {
+		var raid Raids
+		if err := rows.Scan(&raid.raidID, &raid.dungeonName, &raid.raidDate, &raid.raidLeaderID); err != nil {
+			return nil, err
+		}
+		raids = append(raids, raid)
+	}
+	return raids, nil
+}
+
+func createLoot(lootName, lootType string) (int, error) {
+	var id int
+	query := `INSERT INTO loot (lootName, lootType) VALUES ($1, $2) RETURNING loot_id`
+	err := db.QueryRow(query, lootName, lootType).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+	return id, nil
+}
+
+func createRaidMember(raidID int, memberID int, role string) error {
+	query := `INSERT INTO raidMembers (raid_id, member_id, role) VALUES ($1, $2, $3) RETURNING id`
+	var id int
+	err := db.QueryRow(query, raidID, memberID, role).Scan(&id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func isAdmin() bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter admin password: ")
+	pass, _ := reader.ReadString('\n')
+	pass = strings.TrimSpace(pass)
+	return pass == os.Getenv("ADMIN_PASSWORD")
+}
 func isValidNickName(name string) bool {
 	return strings.TrimSpace(name) != ""
 }
 
 func main() {
 	wowDatabase()
+	createClanMember("Wonksta", "Mage", "Fire")
 }
